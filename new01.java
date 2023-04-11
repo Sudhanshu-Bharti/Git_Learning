2e5d3f70b48ea18ea8b056311cd810d940eeb080
Sudhanshu-Bharti <shubham01legend@gmail.com>
2023-04-11 16:25:03 +0530

    Merge branch 'riyank' of https://github.com/Sudhanshu-Bharti/Git_Learning into sudhanshu

--- new01.java
+++ new01.java
@@ -1,9 +1,8 @@
 package Lab_cs102;
 
 public class new01 {
-
      public static void main ( String[] args){
-         System.out.println("Hello SUDHANSHU");
+         System.out.println("  Hello SUDHANSHU");
      }
 
 }
